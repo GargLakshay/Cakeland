3c22e5e9487b1ab4c7b8641cf407d91ae4ec8630
Lakshay Garg <garglakshay048@gmail.com>
2022-01-22 14:21:07 +0530

    initial

---
+++ server.js
@@ -0,0 +1,19 @@
+const express=require('express')
+const app=express()
+const ejs=require('ejs')
+const path=require('path')
+const expressLayout=require('express-ejs-layouts')
+
+app.get("/",function(req,res){
+  res.render("home");
+})
+
+//set template engine
+app.use(expressLayout)
+app.set('views',path.join(__dirname,"/resources/views"))
+app.set('view engine', 'ejs')
+
+const PORT=process.env.PORT||3000;
+app.listen(PORT,function(){
+  console.log("server is running on port 3000");
+})
